ca5b3a900440c31e0f0ca9963d55759b39a481cc
MorningClub <35296910+MorningClub@users.noreply.github.com>
2021-08-13 14:08:48 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MSKEY_VALUE = os.getenv("AZURE_MASTER_KEY")
 
 settings = {
     'host': os.environ.get('ACCOUNT_HOST', 'https://internal-database-test.documents.azure.com:443/'),
-    'master_key': os.environ.get('ACCOUNT_KEY', 'hu81UE1RC9YNzHOrL0zC0tuxVwmyzhYcGiFnzAw6EnGZhstqhetzOa2xUylw3coa5ylE4YYAhDDVW3R4KbqOvw=='),
+    'master_key': os.environ.get('ACCOUNT_KEY', MSKEY_VALUE),
     'database_id': os.environ.get('COSMOS_DATABASE', 'InternalDB'),
     'container_id': os.environ.get('COSMOS_CONTAINER', 'Applications'),
     'case_number_container_id': os.environ.get('COSMOS_CONTAINER', 'CaseNumbers'),
